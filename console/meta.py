5d316a0a461a6f28069e753e197e0d6bbbdfdc43
Gringo Suave <github@mgmiller.net>
2019-09-03 20:18:41 -0700

    bump_version

--- console/meta.py
+++ console/meta.py
@@ -8,7 +8,7 @@ from time import localtime as _localtime
 
 
 pkgname         = 'console'
-__version__     = version = '0.95b2'
+__version__     = version = '0.95b3'
 __author__      = authors = ', '.join([
                                 'Mike Miller',
                                 #~ 'and contributors',
