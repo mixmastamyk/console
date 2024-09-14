d78f2ec47d6a2f77d524d1a4dbee8e454a6a5eab
Gringo Suave <mixmastamyk@bitbucket.org>
2024-09-14 13:46:08 -0700

    init: catch env not found, avoid install issue with pip 24

--- console/__init__.py
+++ console/__init__.py
@@ -2,8 +2,6 @@
     | console - Comprehensive utility library for ANSI terminals.
     | © 2018-2021, Mike Miller - Released under the LGPL, version 3+.
 '''
-import env as _env
-
 from .disabled import empty_bin as _empty_bin, empty_scr_bin as _empty_scr_bin
 
 
@@ -29,59 +27,64 @@ except LookupError:
     _codecs.register(_codec_map.get)
 
 
-# Early terminfo support
-if _env.PY_CONSOLE_USE_TERMINFO.truthy or _env.SSH_CLIENT:
-    try:
-        import curses as _curses
-        # a stub terminfo may have been installed with curses on windows, check
-        from os import name as _os_name
-        if _os_name == 'nt':
-            from _curses import tigetstr as _tigetstr
-            _curses.setupterm()
-            if not _tigetstr('cr'):  # supports carriage return
-                raise ImportError('this curses has a bum terminfo…')
-    except ImportError:
+try:  # avoid install issue with new pip:2024 :-/
+    import env as _env
+except ModuleNotFoundError as err:
+    print(str(err))
+else:
+    # Early terminfo support
+    if _env.PY_CONSOLE_USE_TERMINFO.truthy or _env.SSH_CLIENT:
         try:
-            import jinxed as _curses
+            import curses as _curses
+            # a stub terminfo may have been installed with curses on windows, check
+            from os import name as _os_name
+            if _os_name == 'nt':
+                from _curses import tigetstr as _tigetstr
+                _curses.setupterm()
+                if not _tigetstr('cr'):  # supports carriage return
+                    raise ImportError('this curses has a bum terminfo…')
         except ImportError:
-            raise ImportError(
-                'terminfo not available, try installing ncurses.'
-                ' On Windows, install the package from PyPI named "jinxed".'
-            )
-    _curses.setupterm()
-    using_terminfo = True
-
-
-# defer imports for proper ordering, read using_terminfo
-from .constants import TermLevel as _TermLevel
-from .detection import TermStack
-
-
-# detection is performed if not explicitly disabled
-if (_env.PY_CONSOLE_AUTODETECT.value is None or
-    _env.PY_CONSOLE_AUTODETECT.truthy):
-
-    # detect palette, other modules are dependent
-    from .detection import init as _init
-
-    term_level = _init(using_terminfo=using_terminfo)
-
-    if term_level:  # > 0, may now import other modules
-        ansi_capable = True  # simplify comparisons
-        # monochrome stuff first
-        from .style import fx, defx
-        from .screen import sc
-        from .detection import is_fbterm as _is_fbterm
-
-        if term_level > _TermLevel.ANSI_MONOCHROME:
-            from .style import fg, bg  # Yo Iz, let's do this…
-
-        # curly or colored underlines not handled well by linux consoles:
-        if term_level > _TermLevel.ANSI_BASIC and not _is_fbterm:
-            from .style import ul
+            try:
+                import jinxed as _curses
+            except ImportError:
+                raise ImportError(
+                    'terminfo not available, try installing ncurses. '
+                    'On Windows, install the package from PyPI named "jinxed".'
+                )
         else:
-            fx.curly_underline = fx.underline  # downgrade
-    else:
-        ansi_capable = False  # simplify comparisons
+            _curses.setupterm()
+            using_terminfo = True
 
-    fg, bg, ul, fx, defx, sc, TermStack  # quiet pyflakes
+    # defer imports for proper ordering, read using_terminfo
+    # _TermLevel import was here
+    from .detection import TermStack
+
+    # detection is performed if not explicitly disabled
+    if (_env.PY_CONSOLE_AUTODETECT.value is None or
+        _env.PY_CONSOLE_AUTODETECT.truthy):
+
+        # detect palette, other modules are dependent
+        from .constants import TermLevel as _TermLevel
+        from .detection import init as _init
+
+        term_level = _init(using_terminfo=using_terminfo)
+
+        if term_level:  # > 0, may now import other modules
+            ansi_capable = True  # simplify comparisons
+            # monochrome stuff first
+            from .style import fx, defx
+            from .screen import sc
+            from .detection import is_fbterm as _is_fbterm
+
+            if term_level > _TermLevel.ANSI_MONOCHROME:
+                from .style import fg, bg  # Yo Iz, let's do this…
+
+            # curly or colored underlines not handled well by linux consoles:
+            if term_level > _TermLevel.ANSI_BASIC and not _is_fbterm:
+                from .style import ul
+            else:
+                fx.curly_underline = fx.underline  # downgrade
+        else:
+            ansi_capable = False  # simplify comparisons
+
+        fg, bg, ul, fx, defx, sc, TermStack  # quiet pyflakes
