94f96251af20d362abf16be0504be913582ccbb5
Gringo Suave <mixmastamyk@bitbucket.org>
2025-03-03 14:15:24 -0800

    bump vers

--- console/meta.py
+++ console/meta.py
@@ -14,7 +14,7 @@ from types import SimpleNamespace as _Namespace
 
 pkgname         = 'console'
 full_name       = 'Console'  # rendered for Makefile
-__version__     = version = '0.9911b1'
+__version__     = version = '0.9911'
 __author__      = authors = ', '.join([
                                 'Mike Miller',
                                 #~ 'and contributors',
